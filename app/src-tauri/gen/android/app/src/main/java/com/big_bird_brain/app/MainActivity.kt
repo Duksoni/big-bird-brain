040c3bb4325a6f69b1967fcbecca13d1f0e82050
Dušan Komadinović <dusan.komadinovic20@gmail.com>
2026-05-05 10:39:48 +0200

    Update dependencies for Angular and Rust, add fonts and some icons, update app name and init android project files

---
+++ app/src-tauri/gen/android/app/src/main/java/com/big_bird_brain/app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.big_bird_brain.app
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
